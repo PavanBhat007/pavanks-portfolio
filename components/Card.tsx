c4c355bd44687cc42b81a24737d9995496ff3099
PavanBhat007 <pvn.24.offi@gmail.com>
2026-08-16 19:24:34 +0530

    fix: col-span addition to Card component

--- components/Card.tsx
+++ components/Card.tsx
@@ -3,12 +3,22 @@ import React from "react"
 type CardProps = {
   icon: React.ReactNode,
   title: string,
-  children: React.ReactNode
+  children: React.ReactNode,
+  colSpan?: 1 | 2 | 3 | 4,
 }
 
-export default function Card({ icon, title, children }: CardProps) {
+const colSpanClasses = {
+  1: "col-span-1",
+  2: "col-span-2",
+  3: "col-span-3",
+  4: "col-span-4"
+}
+
+export default function Card({ icon, title, children, colSpan }: CardProps) {
+  const colSpanClass = colSpan ? colSpanClasses[colSpan] : "";
+  
   return (
-    <div className="relative border border-gray-300/20 rounded-xl p-4 group">
+    <div className={`relative border border-gray-300/20 rounded-xl p-4 group overflow-hidden ${colSpanClass}`}>
       <div className="flex items-center gap-2 mb-4 bg-gray-600/10 rounded px-2 py-1 group-hover:bg-gray-400/10 transition-colors duration-300">
         <span className="text-neon">{icon}</span>
         <p className="font-semibold">{title}</p>
